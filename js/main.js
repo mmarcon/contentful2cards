eb340f65f13bceb22250a3f2273a79e7f0e08b59
Massimiliano Marcon <me@marcon.me>
2015-04-24 14:03:50 +0200

    Sorting by creation time and no lazy images.

--- js/main.js
+++ js/main.js
@@ -1,19 +1,14 @@
 ;(function($, M, w, d){
     var CONTENTFUL_PUB_URL = 'https://cdn.contentful.com/spaces/ytivo3qay7d1/entries?access_token=605db5b74362fa9a550103eb2a64e4c75678ea9db025ee622ddcc69766f81879';
 
-    function lazyCacheImage(url, callback) {
-        var image = new Image();
-        image.onload = function(){
-            callback(url);
-        };
-        image.src = url;
-    }
-
     $(function(){
         var template = $('#card-reveal-template').html(), cards = $('.cards');
         M.parse(template);
         $.get(CONTENTFUL_PUB_URL, function(data){
             var rendered = '';
+            data.items = data.items.sort(function(a, b){
+                return (+new Date(b.sys.createdAt)) - (+new Date(a.sys.createdAt));
+            });
             data.items.forEach(function(i){
                 var place = {
                     name: i.fields.name,
@@ -22,12 +17,6 @@
                 rendered += M.render(template, place);
             });
             cards.html(rendered);
-            $('.lazy-image').each(function(_, i){
-                var image = $(i);
-                lazyCacheImage(image.data('src'), function(url){
-                    image.attr('src', url);
-                })
-            });
         });
     });
 }(jQuery, Mustache, window, document));
